4b0ab822932d0b25bb9e39f1862a6ec3ce4138ee
eco5199 <exc9074@mavs.uta.edu>
2025-10-08 18:40:08 -0500

    Added comments in gimbal for clarity

--- template-project/src/subsystems/Gimbal.cpp
+++ template-project/src/subsystems/Gimbal.cpp
@@ -10,17 +10,22 @@ void Gimbal::initialize() {
     MotorYaw.resetEncoderValue();
     targetPitch = 0;
 }
-
+    //Gets the vertical component of the right tick
+    //Applies it to pitch
 void Gimbal::update() {
     int32_t pitchInput = remote.getChannel(tap::communication::serial::Remote::Channel::RIGHT_VERTICAL);
     if (std::abs(pitchInput) > 0) {
         targetPitch += pitchInput * 10; // sensitivity multiplier
         
     }
-
+//this returns a runController inside it 
+        //runController runs the pid controller
+//runControllerDerivativeError
+//accounts for error between desired value and actual real output
     pidPitch.runControllerDerivateError(targetPitch - motorPitch.getEncoderUnwrapped(), 1);
 }
 
+//sets value to motor
 void Gimbal::tick(float scale) {
     motorPitch.setDesiredOutput(static_cast<int32_t>(pidPitch.getOutput() * scale));
 }
