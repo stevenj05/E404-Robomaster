8ceddc5932d9f09f430242cc5934f720b2451424
Turtgun <icisraelcarrillo@gmail.com>
2025-10-08 15:50:28 -0500

    clearer name for gimbal pitch

--- template-project/src/subsystems/Gimbal.cpp
+++ template-project/src/subsystems/Gimbal.cpp
@@ -6,16 +6,16 @@ Gimbal::Gimbal(tap::communication::serial::Remote& remoteIn, double& _yaw, doubl
 void Gimbal::initialize() {
     motorPitch.initialize();
     motorPitch.resetEncoderValue();
-    targetPos = 0;
+    targetPitch = 0;
 }
 
 void Gimbal::update() {
     int32_t pitchInput = remote.getChannel(tap::communication::serial::Remote::Channel::RIGHT_VERTICAL);
     if (std::abs(pitchInput) > 0) {
-        targetPos += pitchInput * 10; // sensitivity multiplier
+        targetPitch += pitchInput * 10; // sensitivity multiplier
     }
 
-    pidPitch.runControllerDerivateError(targetPos - motorPitch.getEncoderUnwrapped(), 1);
+    pidPitch.runControllerDerivateError(targetPitch - motorPitch.getEncoderUnwrapped(), 1);
 }
 
 void Gimbal::tick(float scale) {
